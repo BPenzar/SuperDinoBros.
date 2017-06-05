e054949436f0da0914c799f8190f163b6d850553
BPenzar <bruno.penzar@posteo.de>
2017-06-05 14:08:39 +0200

    Test Projekt, Alphaversion des Spiels.

---
+++ Code/Test_Project/Assets/Scripts/followPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Kamera folgt Player Script
+public class followPlayer : MonoBehaviour {
+
+	public GameObject player;       
+
+	private Vector3 offset;   
+	private Vector3 zOffset;   
+
+	private Vector3 moveVector;
+
+
+	void Start () 
+	{
+		offset = transform.position - player.transform.position;
+		zOffset.z = offset.z;
+
+	}
+		
+	void LateUpdate () 
+	{
+		moveVector = player.transform.position + offset;
+
+		//Y
+		moveVector.y = Mathf.Clamp(moveVector.y,2,2);
+		//Z
+		moveVector.z = zOffset.z;	
+
+		transform.position = moveVector;
+	}
+}
---
+++ Code/Test_Project/Assets/Scripts/movePlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePlayer : MonoBehaviour {
+
+	private CharacterController controller;
+	public float speed = 5.0f;
+	private Vector3 moveVector;
+	public float standardgeschwindigkeit = 5.0f;
+
+	private float vertivalVelocity = 0.0f;
+	private float gravity = 30.0f;
+
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+		
+	void Update () {
+		moveVector = Vector3.zero;
+
+
+		//X-Achse (Rechts/Links)
+		// Geschwindigkeit mit Pfeilen (rechts/links) mitbestimmen
+		moveVector.x = standardgeschwindigkeit*2 + Input.GetAxisRaw("Horizontal") * speed;
+
+		//Y-Achse (Hoch/Runter)
+		//Springen
+		if (controller.isGrounded) 
+		{
+			vertivalVelocity = -0.5f;	
+			if(Input.GetKeyDown(KeyCode.Space)){
+				vertivalVelocity += 15f;
+			}
+		}
+		//Fallen
+		else 
+		{
+			vertivalVelocity -= gravity *Time.deltaTime;
+		}
+		moveVector.y = vertivalVelocity;
+	
+
+		// Bewegung aktualisieren
+		controller.Move (moveVector * Time.deltaTime);
+	}
+
+	// Public Funktion zum Beschleunigen/Entschleunigen (genutzt im Score.cs Script)
+	public void SetSpeed (int modifier){
+		standardgeschwindigkeit = 5.0f + modifier;
+	}
+}
+
