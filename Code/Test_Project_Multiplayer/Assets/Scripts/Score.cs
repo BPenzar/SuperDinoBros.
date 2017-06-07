7ac0bf18d7a94ab8a7be04f5cc6915101e544721
BPenzar <bruno.penzar@posteo.de>
2017-06-07 13:28:13 +0200

    Test Projekt, Alphaversion des Spiels, Multiplayer

---
+++ Code/Test_Project_Multiplayer/Assets/Scripts/Score.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	private float score = 0.0f;
+	public Text scoreText;
+
+	private int schwierigkeit = 1;
+	private int schwierigkeitMax = 10;
+	private int punkteBisNaechstesLevel = 100;
+
+	void Start () {
+		scoreText.text = "Highscore";
+	}
+	
+	// Highscore (in Abhängigkeit der Position des Spielers)
+	void Update () {
+
+
+		if (score >= punkteBisNaechstesLevel) {
+			LevelUp ();
+		}
+
+		//Score = X-Position Player
+		score = GameObject.FindGameObjectWithTag ("Player").transform.position.x;
+		scoreText.text = ((int)score).ToString ();
+	}
+
+
+	void LevelUp (){
+		// Debug.Log (schwierigkeit);
+		// Debug.Log (punkteBisNaechstesLevel);
+
+		if (schwierigkeit == schwierigkeitMax)
+			return;
+		
+		punkteBisNaechstesLevel *= 2;
+		schwierigkeit++;
+		GetComponent<movePlayer>().SetSpeed (schwierigkeit);
+
+		// Debug.Log (schwierigkeit);
+	}
+}
---
+++ Code/Test_Project_Multiplayer/Assets/Scripts/followPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Kamera folgt Player Script
+public class followPlayer : MonoBehaviour {
+
+	private Transform player;
+
+	private Vector3 offset;   
+	private Vector3 zOffset;   
+
+	private Vector3 moveVector;
+
+
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		offset = transform.position - player.position;
+		zOffset.z = offset.z;
+
+	}
+		
+	void LateUpdate () 
+	{
+		moveVector = player.transform.position + offset;
+
+		//Y
+		moveVector.y = Mathf.Clamp(moveVector.y,2,2);
+		//Z
+		moveVector.z = zOffset.z;	
+
+		transform.position = moveVector;
+	}
+}
