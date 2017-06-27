8248827d41d6e8d78e0987abb0da8b219987f32e
BPenzar <bruno.penzar@posteo.de>
2017-06-27 15:42:09 +0200

    Singleplayer_Alpha

---
+++ Unity_Code/Singleplayer_Alpha/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public Text highscoreText;
+
+	void Start () {		
+		highscoreText.text = "Highscore: " + ((int)PlayerPrefs.GetFloat ("Highscore")).ToString ();
+	}
+
+	public void ToSingleplayer(){
+		SceneManager.LoadScene ("Singleplayer");
+	}
+}
